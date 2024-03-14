3a24762f9810f24a45430ba7cbe6d4fe9ced4b00
maryamHabibpourr <Einesheikh_12@yahoo.com>
2024-03-14 08:29:30 +0330

    implement  pricing and Roadmap and footer to the  project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import ButtonGradient from './assets/svg/ButtonGradient';
 import Benefits from './components/Benefits';
 import Collaboration from './components/Collaboration';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Pricing from './components/Pricing';
+import PricingList from './components/PricingList';
+import Roadmap from './components/Roadmap';
 import Services from './components/Services';
 
 function App() {
@@ -14,6 +18,9 @@ function App() {
         <Benefits />
         <Collaboration/>
         <Services />
+        <Pricing/>
+        <Roadmap/>
+        <Footer/>
       </div>
       <ButtonGradient />
     </>
